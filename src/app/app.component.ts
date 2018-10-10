d438e7a564b48c8e26227d0be98bcbd3eadd582b
Alexis Grisham <alexiskjg16@gmail.com>
2018-10-09 20:16:30 -0400

    createdtacoapp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'hello-world';
+  constructor(private http:HttpClient){
+
+  }
+  title = 'TACOS';
+  taco={}
+  public getTaco(){
+    this.http.get("http://taco-randomizer.herokuapp.com/random/?full-taco=true")
+    .subscribe(function(taco){this.taco=taco}.bind(this))
+  }
 }
+
